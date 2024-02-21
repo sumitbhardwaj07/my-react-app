4b90443bf04d08a81fc90b30bf67fe2db8d31dd7
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-02-21 15:21:36 +0530

    add expenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return <>
+        <h4>Expense Items</h4>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+    </>
+}
+
+export default ExpenseItem;
