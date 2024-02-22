2707370f172f4049f1b9107879b8805d1244ad2c
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-02-22 11:37:05 +0530

    use of map

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
-function ExpenseItem(){
-    return <>
-        <h4>Expense Items</h4>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
-    </>
+import "./ExpenseItem.css";
+function ExpenseItem(props){
+    return (
+    <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">Rs{props.amount}</div>
+        </div>
+    </div>
+    );
 }
 
 export default ExpenseItem;
