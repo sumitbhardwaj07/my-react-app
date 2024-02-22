f746a1df8680372560e651c0fda0f6277aad720f
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-02-22 13:10:42 +0530

    add css to date

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ const expenses = [
     title: 'Toilet Paper',
     amount: 94.12,
     date: new Date(2020, 7, 14),
+    location: 'New Delhi',
+
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location: 'Rohtak',},
   {
     id: 'e3',
     title: 'Car Insurance',
     amount: 294.67,
     date: new Date(2021, 2, 28),
+    location: 'Jind',
   },
   {
     id: 'e4',
     title: 'New Desk (Wooden)',
     amount: 450,
     date: new Date(2021, 5, 12),
+    location: 'Mathura',
   },
 ];
 function App() {
@@ -30,6 +34,7 @@ function App() {
         title={expense.title}
         amount={expense.amount}
         date={expense.date}
+        location={expense.location}
         ></ExpenseItem>
       ))}
     </div>
