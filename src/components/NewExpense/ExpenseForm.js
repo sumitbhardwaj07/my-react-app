6202c29139e634069fc000d0d4dcf8787cc0e49b
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-02-25 12:24:53 +0530

    create an object after click on submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,49 @@ import React ,{useState} from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    const [enteredTitle,setEnteredTitle] = useState('');
-    const [enteredAmount,setEnterdAmount] = useState('');
-    const [enteredDate,setEnteredDate] = useState('');
+    // const [enteredTitle,setEnteredTitle] = useState('');
+    // const [enteredAmount,setEnterdAmount] = useState('');
+    // const [enteredDate,setEnteredDate] = useState('');
+    const[userInput,setUserInput] = useState({
+        enteredTitle : '',
+        enteredAmount : '',
+        enteredDate : '',
+
+    })
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
+        //setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value,
+        })
     };
     const amountChangeHandler = (event) => {
-        setEnterdAmount(event.target.value);
+        //setEnterdAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        })
     };
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
+        //setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        })
     };
 
-    return <form>
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData={
+            title: userInput.enteredTitle,
+            amount: userInput.enteredAmount,
+            date: new Date(userInput.enteredDate),
+        };
+
+        console.log(expenseData);
+    }
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
