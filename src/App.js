17aa9de34a441d935fe11aa0fa99712defa09c9c
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-02-25 16:03:49 +0530

    child to parent communication

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import NewExpense from './components/NewExpense/NewExpense'
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
-const expenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-    location: 'New Delhi',
-
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location: 'Rohtak',},
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-    location: 'Jind',
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-    location: 'Mathura',
-  },
-];
 const App = ()=> {
-  return (
-    <div>
-      <NewExpense />
-      <Expenses items={expenses}/>
-    </div>
-  );
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: 'New Delhi',
+  
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location: 'Rohtak',},
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: 'Jind',
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: 'Mathura',
+    },
+  ];
+
+  const addExpenseHandler = expense =>{
+    console.log('In App.js');
+    console.log(expense);
+  }
+  
+    return (
+      <div>
+        <NewExpense onAddExpense={addExpenseHandler}/>
+        <Expenses items={expenses}/>
+      </div>
+    );
 }
 
 export default App;
